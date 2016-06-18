40b31a79d388d214976a4148318fbb7238273bb0
jedbrun@hotmail.com <jedbrun@hotmail.com>
2016-06-18 17:26:25 -0500

    ready for nav

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 /**
  * Created by jed on 6/18/2016.
  */
+
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('client/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('client/css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('client/scss/*.scss', ['sass']);
+});
