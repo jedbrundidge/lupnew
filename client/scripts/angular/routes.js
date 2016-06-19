ec4a06b9761b8fee7bbb653a9896a4ff9727ff4a
jedbrun@hotmail.com <jedbrun@hotmail.com>
2016-06-19 02:20:09 -0500

    routes done

--- client/scripts/angular/routes.js
+++ client/scripts/angular/routes.js
@@ -1,3 +1,33 @@
 /**
  * Created by jed on 6/19/2016.
  */
+
+var lupus = angular.module('lupus', ['ngRoute']);
+
+lupus.config(function ($routeProvider) {
+    $routeProvider
+        
+        .when('/home', {
+            templateUrl: '/views/home.html'
+        })
+    
+        .when('/register', {
+            templateUrl: '/views/register.html'
+        })
+    
+        .when('/teams', {
+            templateUrl: '/views/teams.html'
+        })
+
+        .when('/skips', {
+            templateUrl: '/views/skips.html'
+        })
+
+        .when('/donate', {
+            templateUrl: '/views/donate.html'
+        })
+
+        .when('/directions', {
+            templateUrl: '/views/directions.html'
+        })
+});
